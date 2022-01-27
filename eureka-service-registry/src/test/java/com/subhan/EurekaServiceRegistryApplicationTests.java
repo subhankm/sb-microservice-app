3ba0b9156ccaf5b48b5c32773891adaf74432269
subhankm <mabusubhan.k@gmail.com>
2022-01-27 08:48:42 +0530

    added eureka-server-setup for service registry

---
+++ eureka-service-registry/src/test/java/com/subhan/EurekaServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subhan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
